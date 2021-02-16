886fb095d05bfbf3bd7223c2889e4e17dc5c532e
Anton <tymoshenko.a@gmail.com>
2021-02-16 15:51:13 +0200

    homework 11

---
+++ app/books/management/commands/generate_authors_data.py
@@ -0,0 +1,27 @@
+import random
+from django.core.management.base import BaseCommand
+from books.models import Author
+from faker import Faker
+
+
+class Command(BaseCommand):
+    help = 'Generate Random Authors Data'  # noqa
+
+    def handle(self, *args, **options):
+        fake = Faker()
+
+        for _ in range(10):
+            first_name = fake.first_name()
+            second_name = fake.last_name()
+            country = fake.country()
+            gender = random.choice([True, False])
+            native_language = country
+
+            Author.objects.create(
+                first_name=first_name,
+                second_name=second_name,
+                country=country,
+                gender=gender,
+                native_language=native_language
+
+            )
